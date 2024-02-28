a08964e213b1ac78bb1d82e223a27ffebabf79b2
Giacomo Marcon <marcongiacomo@gmail.com>
2024-02-28 17:59:39 +0100

    works, check performance against original

--- prisoner_dilemma.py
+++ prisoner_dilemma.py
@@ -5,38 +5,33 @@ class Strategy:
     '''
     object to represent a strategy in the iterated prisoner's dilemma game
     '''
-    def __init__(self, name: str, action_function, action_func_trans, prevConsidered: int=1):
+    def __init__(self, name: str, action_function, action_func_parameters: dict = {}):
         '''
         name: string
-        prevConsidered: int, number of previous moves to consider
         action_function: function, takes in a np.array of shape (n, 2) where n is the number of moves played so far by the opponent
-        action_func_trans: behaviur of the action function when the number of moves played by the opponent is less than prevConsidered: 
-            - function to specify the behaviour
-            - 'Nice' (default): always cooperate
-            - 'Nasty': always defect
         '''
         self.name: str = name
-        self.prevConsidered = prevConsidered
         self.action_func = action_function
-        self.action_func_trans = action_func_trans
-        self.nice_trans = False
-        self.nasty_trans = False
+        self.action_func_parameters = action_func_parameters
     def __repr__(self):
         return self.name
 
-    def action(self, prevActions: np.array, action_func_parameters: dict):
+    def action(self, prevActions: np.array):
         '''
         Carries out the decision making process for the strategy for the singular move
-        prevActions: np.array of shape (n,2) where n is the number of moves played so far
         '''
-        moveNum = np.shape(prevActions)[1]
-        # normal behaviour
-        if(moveNum >= self.prevConsidered):
-            return self.action_func(prevActions, **action_func_parameters)
-        # transition behaviour
-        else: 
-            return self.action_func_trans(prevActions, **action_func_parameters)
-
+        return self.action_func(prevActions, **self.action_func_parameters)
+    
+    def edit_af_params(self, new_params: dict):
+        '''
+        edits the parameters of the action function
+        '''
+        for (k, v) in new_params.items():
+            if k in self.action_func_parameters.keys():
+                self.action_func_parameters[k] = v
+            else:
+                print("Strategy.edit_af_params: parameter not found")
+                return -1
 class Match: 
     '''
     object to represent a match between two strategies
@@ -50,23 +45,22 @@ class Match:
                  score_singleCoop_cooperant: int = 0,
                  score_singleCoop_defectant: int = 3,
                  score_bothCoop: int = 2,
-                 score_bothDef: int = 1,
-                 action_func_parameters: dict = {}):
-        
+                 score_bothDef: int = 1):
+        # parameters
         self.strat1 = strat1
         self.strat2 = strat2
-        self.is_numRound_random = False
         self.score_singleCoop_cooperant = score_singleCoop_cooperant
         self.score_singleCoop_defectant = score_singleCoop_defectant
         self.score_bothCoop = score_bothCoop
         self.score_bothDef = score_bothDef
-        self.action_func_parameters = action_func_parameters
+        self.verbose = verbose
+        self.repetitions = repetitions
+        self.score = np.zeros((repetitions, 2))
+        # setup to allow for random number of rounds
+        self.is_numRound_random = False
         if sigma_randomRounds is not False:
             self.is_numRound_random = True
             self.sigma_randomRound = sigma_randomRounds
-        self.repetitions = repetitions
-        self.verbose = verbose
-        self.score = np.zeros((repetitions, 2))
         if self.is_numRound_random is False:
             self.strat1_actions = np.zeros((repetitions, numRounds))
             self.strat2_actions = np.zeros((repetitions, numRounds))
@@ -85,17 +79,15 @@ class Match:
                 self.strat1_actions = np.zeros((self.repetitions, self.numRounds[i]))
                 self.strat2_actions = np.zeros((self.repetitions, self.numRounds[i]))
             for j in range(self.numRounds[i]):
+                # formatting of the previous moves to feed into the action function
                 strat1_prevMoves = np.vstack((self.strat1_actions[i, :j], self.strat2_actions[i, :j]))
                 strat2_prevMoves = np.vstack((self.strat2_actions[i, :j], self.strat1_actions[i, :j]))
-                strat1_action = self.strat1.action(strat1_prevMoves, self.action_func_parameters)
-                strat2_action = self.strat2.action(strat2_prevMoves, self.action_func_parameters)
+                # action
+                strat1_action = self.strat1.action(strat1_prevMoves)
+                strat2_action = self.strat2.action(strat2_prevMoves)
                 self.strat1_actions[i, j] = strat1_action
                 self.strat2_actions[i, j] = strat2_action
-                # if(self.verbose):
-                #     print("Round: ", j)
-                #     print("Strat1 (", self.strat1.name, ") : ", strat1_action)
-                #     print("Strat2 (", self.strat2.name, ") : ", strat2_action)
-                # scoring
+                # score evaluation
                 if(strat1_action == 0 and strat2_action == 0):
                     self.score[i, 0] += self.score_bothDef
                     self.score[i, 1] += self.score_bothDef
--- strategies.py
+++ strategies.py
@@ -1,7 +1,10 @@
 import numpy as np
 import prisoner_dilemma as prd
 def e4e_af(prevActions: np.array, **kwargs):
-    return prevActions[1, -1]  
+    if np.size(prevActions) == 0:
+        return np.array([1])
+    else: 
+        return prevActions[1, -1]  
 
 def nice_af(prevActions: np.array, **kwargs):
     return np.array([1])
@@ -12,41 +15,55 @@ def nasty_af(prevActions: np.array, **kwargs):
 def random_af(prevActions: np.array, **kwargs):
     return np.array([np.random.randint(0, 2)])
 
-def probing_af(prevActions: np.array, **kwargs):
-    self_first_defect = False
-    opp_first_defect = False
-    for i in range(np.shape(prevActions)[1]):
-        if prevActions[0, i] == 0:
-            self_first_defect = True
-            break
-        if prevActions[1, i] == 0:
-            opp_first_defect = True
-            break 
-    if(self_first_defect or not (self_first_defect or opp_first_defect)):
-        opp_tot_defect = np.shape(prevActions)[1] - np.sum(prevActions[1])
-        if opp_tot_defect/np.shape(prevActions)[1] < 0.5:
-            if np.random.rand(1)[0] < 0.5:
-                return np.array([0])
-    return prevActions[1, -1]
-
-def grudge_af(prevActions: np.array, **kwargs):
-    if np.sum(prevActions[1]) != np.shape(prevActions)[1]:
-        return np.array([0])
-    else:
+def probing_af(prevActions: np.array, memory = 10, **kwargs):
+    if np.shape(prevActions)[1] < memory:
         return np.array([1])
+    else: 
+        self_first_defect = False
+        opp_first_defect = False
+        for i in np.arange(-memory, 0):
+            if prevActions[0, i] == 0:
+                self_first_defect = True
+                break
+            if prevActions[1, i] == 0:
+                opp_first_defect = True
+                break 
+        if(self_first_defect or not (self_first_defect or opp_first_defect)):
+            opp_tot_defect = memory - np.sum(prevActions[1, -memory:])
+            if opp_tot_defect/memory < 0.5:
+                if np.random.rand(1)[0] < 0.5:
+                    return np.array([0])
+        return prevActions[1, -1]
+
+def grudge_af(prevActions: np.array, memory = 10, **kwargs):
+    if np.shape(prevActions)[1] == 0: # first move 
+        return np.array([1])
+    elif np.shape(prevActions)[1] < memory: # moves < memory the logic breaks lol and np.shape(prevActions)[1] must be used instead of memory
+        if np.sum(prevActions[1, -np.shape(prevActions)[1]:]) != np.shape(prevActions)[1]:
+            return np.array([0])
+        else:
+            return np.array([1])
+    else: # works nicely 
+        if np.sum(prevActions[1, -memory:]) != memory:
+            return np.array([0])
+        else:
+            return np.array([1])
 
 def nice_e4e_af(prevActions: np.array, **kwargs):
-    if prevActions[1, -1] == 0 and prevActions[1, -2]:
-        return np.array([0])
-    else: 
+    if np.shape(prevActions)[1] < 2:
         return np.array([1])
+    else:
+        if prevActions[1, -1] == 0 and prevActions[1, -2]:
+            return np.array([0])
+        else: 
+            return np.array([1])
 
-e4e = prd.Strategy("e4e", e4e_af, nice_af) 
-random = prd.Strategy("Random", random_af, random_af)
-nice = prd.Strategy("Nice", nice_af, nice_af)
-nasty = prd.Strategy("Nasty", nasty_af, nasty_af)
-probing_5 = prd.Strategy("probing_5", probing_af, nice_af, prevConsidered=5)
-probing_10 = prd.Strategy("probing_10", probing_af, nice_af, prevConsidered=10)
-grudge_5 = prd.Strategy("grudge_5", grudge_af, nice_af, prevConsidered=5)
-grudge_10 = prd.Strategy("grudge_10", grudge_af, nice_af, prevConsidered=10)
-nice_e2e = prd.Strategy("nice_e4e", nice_e4e_af, nice_af, prevConsidered=2)
+e4e = prd.Strategy("e4e", e4e_af) 
+random = prd.Strategy("Random", random_af)
+nice = prd.Strategy("Nice", nice_af)
+nasty = prd.Strategy("Nasty", nasty_af)
+nice_e2e = prd.Strategy("nice_e4e", nice_e4e_af)
+probing_10 = prd.Strategy("probing_10", probing_af, action_func_parameters={"memory": 10})
+grudge_10 = prd.Strategy("grudge_10", grudge_af, action_func_parameters={"memory": 10})
+probing_5 = prd.Strategy("probing_5", probing_af, action_func_parameters={"memory": 5})
+grudge_5 = prd.Strategy("grudge_5", grudge_af, action_func_parameters={"memory": 5})
